6ddcb69c18c46908fba621fe845459d5e838ee2b
mr-guo <1367060166@qq.com>
2017-06-26 19:54:39 +0800

    commit to save it

--- src/main/java/gq/wh/WBMulti.java
+++ src/main/java/gq/wh/WBMulti.java
@@ -5,13 +5,13 @@ package gq.wh;
  * Created by mr-guo on 2017/6/26.
  */
 public class WBMulti {
-    final static int ADDTIMES = 4;
-    final static int MULTIPLY = 4;
+    public final static int ADDTIMESTIMES = 4;
+    public final static int MULTIPLYTIMES = 4;
     Box10to5[] partialAdd; //abb 4 bit with 4 bit ,one bit for carry
     Box8to8[] partialMulti;//multiply by 4 * 4
 
     public WBMulti(){
-        partialAdd = new Box10to5[ADDTIMES];
-        partialMulti = new Box8to8[MULTIPLY];
+        partialAdd = new Box10to5[ADDTIMESTIMES];
+        partialMulti = new Box8to8[MULTIPLYTIMES];
     }
 }
